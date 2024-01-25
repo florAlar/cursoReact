733836ebc0017e862644746e79de0aaaf1e98a96
floralar <flor.alarcos@gmail.com>
2024-01-25 09:10:06 -0300

    adicion de menu en Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from './Hero'
 import ItemListConteiner from './ItemListConteiner'
+import MenuHome from './MenuHome'
 
 
 const Home = () => {
   return (
     <div>
         <div><Hero/></div>
+        <div><MenuHome/></div>
         
         <div><ItemListConteiner/></div>
     </div>
--- src/components/MenuHome.jsx
+++ src/components/MenuHome.jsx
@@ -1,13 +1,35 @@
-import { Box } from '@chakra-ui/react'
+import { Spacer, Stack, Flex } from '@chakra-ui/react'
 import React from 'react'
+import { Link } from 'react-router-dom'
+import '../App.css'
 
 const MenuHome = () => {
   return (
-    <Box className='horizontal'>
-        <img  src="../src/assets/accesorios.png" alt="" />
-        <img src="../src/assets/indumentaria.png" alt="" />
-        <img src="" alt="" />
-    </Box>
+    <Stack className='horizontal'>
+      <div width='100%' className='bannerPostHero'>
+        <p>ENCONTRA LAS MEJORES MARCAS</p>
+      </div>
+      <Flex mt='50px' alignItems='center'>
+      <Spacer />
+      <Link to='/category/Honda'>
+      <img className='imgHomeMenu' width='150px' src="../src/assets/logoHonda.png" alt="" />
+      </Link>
+      <Spacer />
+      <Link to='/category/Kawasaki'>
+      <img className='imgHomeMenu' width='150px' src="../src/assets/logoKawa.png" alt="" />
+      </Link>
+      <Spacer />
+      <Link to='/category/Yamaha'>
+      <img className='imgHomeMenu' width='150px' src="../src/assets/logoYamaha.png" alt="" />
+      </Link>
+      <Spacer />
+      <Link to='/category/Harley Davidson'>
+      <img className='imgHomeMenu' width='150px' src="../src/assets/logoHarley.png" alt="" />
+      </Link>
+      <Spacer />
+      
+      </Flex>
+    </Stack>
   )
 }
 
